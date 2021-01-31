ad3deef4f976592ef8d8880275b0a31e88b9fa87
Vendicated <venneptury@protonmail.com>
2021-01-31 02:16:55 +0100

    Add installer script

---
+++ installer.sh
@@ -0,0 +1,33 @@
+#/!usr/bin/env sh
+
+die() {
+	echo "$1"
+	exit
+}
+
+sync_dotfiles() {
+	for item in $(ls .config); do
+		cp -r "~/.config/$item" ".config/$item"
+	done
+
+	git commit -am "Update dotfiles"
+	git push
+}
+
+install_dotfiles() {
+	[ -d .git ] && git pull --quiet
+
+	for item in $(ls .config); do
+		cp -r ".config/$item" "~/.config/$item"
+	done
+}
+
+[ -d .config ] || die ".config directory not found. Make sure you run this from the correct directry"
+
+if [ "$1" == "sync" ]; then
+	sync_dotfiles
+elif [ "$1" == "install" ]; then
+	install_dotfiles
+else
+	echo "Usage: $0 <install | sync>"
+fi
