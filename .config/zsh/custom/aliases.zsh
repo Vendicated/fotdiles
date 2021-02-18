fa88b164ba0b84b7901a81886845872a0717e006
Vendicated <venneptury@protonmail.com>
2021-02-18 04:39:07 +0100

    Update dotfiles

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -2,14 +2,28 @@ alias vimdiff="nvim -d"
 alias vim="nvim"
 alias vi="nvim"
 alias lolcat="lolcat --truecolor"
-alias neofetch="neofetch | lolcat"
 alias shutdown="shutdown now"
 alias rg="rg --no-ignore --hidden"
+alias pin="yay -S"
+alias pun="yay -Rs"
+alias pup="yay -Syu"
+alias src="source $HOME/.zshrc"
 
 sxivremote() { 
   curl -sS "${1:?Please specify an url}" -o /tmp/sxivremote && 
     sxiv /tmp/sxivremote && 
     rm -f /tmp/sxivremote 
+}
+
+uwu() {
+  trap "CANCELUWU=1" SIGINT
+  while [ -z $CANCELUWU ]; do
+    echo -ne "\r\033[0KUwU" | lolcat 2> /dev/null || break
+    sleep .05
+  done
+  
+  echo -e "\r\033[0KUwU" | lolcat 2> /dev/null
+  unset CANCELUWU
 }
 
 codi() {
