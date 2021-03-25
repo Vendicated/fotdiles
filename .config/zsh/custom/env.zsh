ee257c6ac1522a933d54e43ccf8996810e44113d
Vendicated <vendicated@riseup.net>
2021-03-25 04:21:25 +0100

    Update dotfiles

--- .config/zsh/custom/env.zsh
+++ .config/zsh/custom/env.zsh
@@ -1,1 +1,8 @@
 export FZF_DEFAULT_COMMAND="rg --files --hidden"
+export EDITOR=nvim
+export VISUAL=nvim
+export TERMINAL=alacritty
+export BROWSER=firefox
+export ZSH=$HOME/.oh-my-zsh
+export ZSH_CUSTOM=$HOME/.config/zsh/custom
+export PATH=$HOME/.yarn/bin:$HOME/.local/bin:$HOME/.cargo/bin:$PATH
