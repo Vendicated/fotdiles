871c7821cbb6a301231776fba1f9379c89d3106a
Vendicated <vendicated@riseup.net>
2021-04-12 01:21:42 +0200

    Improve installer and make it posix compliant

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
-#/!usr/bin/env sh
+#!/bin/sh
+set -e
 
 die() {
 	echo "$1"
@@ -7,27 +8,41 @@ die() {
 
 # Usage: SourceDirectory TargetDirectory
 link_all() {
-	for file in $(ls "$1"); do
-		[[ -e "$2/$file" ]] && rm -rf "$2/$file"
-		ln -s "$1/$file" "$2/$file"
+	for file in "$1/"*; do
+		outfile="${HOME:?WHY IS THE HOME VARIABLE NOT SET WHAT THE FUCK}/${file#*$PWD/}"
+		if [ -e "$outfile" ]; then
+			if [ -n "$_override" ]; then
+				echo "$outfile exists. Deleting since override command was used"
+				rm -rf "$outfile"
+			else 
+				echo "$outfile exists. Run $0 override to override"
+				continue
+			fi
+		fi
+		ln -s "$file" "$outfile"
 	done
 }
 
 install_dotfiles() {
-	git pull --quiet
-
 	mkdir -p "$HOME/.config"
 	mkdir -p "$HOME/.local/bin"
 
-	link_all "$PWD/.config" "$HOME/.config"
-	link_all "$PWD/.local/bin" "$HOME/.local/bin"
+	link_all "$PWD/.config" "$HOME"
+	link_all "$PWD/.local/bin" "$HOME"
 }
 
-[ -d .git ] || die ".git directory not found. Make sure you run this from the correct directry"
-[ -d .config ] || die ".config directory not found. Make sure you run this from the correct directory"
-[ -d .local/bin ] || die ".local/bin directory not found. Make sure you run this from the correct directory"
+check_dir() {
+	[ -d "$1" ] || die "$1 not found or not a directory. Make sure you run this from the correct directory"
+}
 
-if [ "$1" == "install" ]; then
+check_dir .git
+check_dir .config
+check_dir .local
+
+if [ "$1" = "install" ]; then
+	install_dotfiles
+elif [ "$1" = "override" ]; then
+	_override=1
 	install_dotfiles
 else
 	echo "Tool to symlink everything inside .config and .local/bin"
